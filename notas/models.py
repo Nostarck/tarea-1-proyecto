fa6db5aa8f5971cbd1ce90e3b99e77ace07b3203
Joshua Philippe Molina Van Den Bergh <72298406+Nostarck@users.noreply.github.com>
2021-08-20 21:18:53 -0600

    CRG-3 fail test

--- notas/models.py
+++ notas/models.py
@@ -4,6 +4,6 @@ from django.db.models.fields import CharField, TextField
 
 class Note(models.Model):
     title = CharField(max_length=100)
-    note = TextField()
+    note = TextField(
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
     modified_time = models.TimeField(auto_now=True)
