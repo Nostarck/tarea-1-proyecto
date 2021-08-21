809d57a266a62255a0512b7dce356854faa76abe
Joshua Philippe Molina Van Den Bergh <72298406+Nostarck@users.noreply.github.com>
2021-08-20 21:35:28 -0600

    Delete notas directory

--- notas/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-from django.db.models.fields import CharField, TextField
-
-class Note(models.Model):
-    title = CharField(max_length=100)
-    note = TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
-    modified_time = models.TimeField(auto_now=True)
